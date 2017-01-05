d878c786237205f1f81beaca8d0b020a4a2b9ac2
ssthouse <ssthouse@163.com>
2017-01-05 22:56:25 +0800

    Finish learning 迭代器

--- test_generator/demo_iterable.py
+++ test_generator/demo_iterable.py
@@ -3,7 +3,8 @@ import random
 
 
 class demo_iterable(object):
-    # 实现__next__方法, 使其可以被 next() 内建方法调用
+    # 1. 实现__next__方法, 使其可以被 next() 内建方法调用
+    # 2. 通过实现这个方法, 可以处理未知的事件, 并且可以在迭代过程中随时停止迭代, 比直接 for in 列表要灵活的多
     def __next__(self):
         return self.next()
 
@@ -14,6 +15,7 @@ class demo_iterable(object):
         else:
             return value
 
+    # 实现迭代器接口, 使其可被 for in 调用
     # 创建自身 并返回
     def __iter__(self):
         return demo_iterable()
